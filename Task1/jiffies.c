acce7ca471e12890cf3c9e92bd15b4b5abeb3770
VermaAdi <97848600+VermaAdi@users.noreply.github.com>
2024-03-17 22:35:39 -0400

    Create jiffies.c

---
+++ Task1/jiffies.c
@@ -0,0 +1,80 @@
+/**
+ * Kernel module that communicates with /proc file system.
+ * 
+ * The makefile must be modified to compile this program.
+ * Change the line "simple.o" to "jiffies.o"
+ *
+ * Operating System Concepts - 10th Edition
+ * Copyright John Wiley & Sons - 2018
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <asm/uaccess.h>
+#include <linux/jiffies.h>
+
+#define BUFFER_SIZE 128
+
+#define PROC_NAME "jiffies"
+
+/**
+ * Function prototypes
+ */
+static ssize_t proc_read(struct file *file, char *buf, size_t count, loff_t *pos);
+
+static struct proc_ops proc_ops = {
+        .proc_read = proc_read,
+};
+
+/* This function is called when the module is loaded. */
+static int proc_init(void)
+{
+
+        // creates the /proc/jiffies entry
+        // the following function call is a wrapper for
+        // proc_create_data() passing NULL as the last argument
+        proc_create(PROC_NAME, 0, NULL, &proc_ops);
+
+        printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
+
+	return 0;
+}
+
+/* This function is called when the module is removed. */
+static void proc_exit(void) {
+
+        // removes the /proc/jiffies entry
+        remove_proc_entry(PROC_NAME, NULL);
+
+        printk( KERN_INFO "/proc/%s removed\n", PROC_NAME);
+}
+
+static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
+{
+    int rv = 0;
+    char jiffies_str[BUFFER_SIZE];
+    static int completed = 0;
+    
+    if (completed) {
+        completed = 0;
+        return 0;
+    }
+    
+    completed = 1;
+    
+    rv = snprintf(jiffies_str, sizeof(jiffies_str), "%lu\n", jiffies);
+    copy_to_user(usr_buf, jiffies_str, rv);
+    
+    return rv;
+}
+
+
+/* Macros for registering module entry and exit points. */
+module_init( proc_init );
+module_exit( proc_exit );
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Jiffies Module");
+MODULE_AUTHOR("superuser");
